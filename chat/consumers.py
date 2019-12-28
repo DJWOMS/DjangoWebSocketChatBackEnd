0f9a9bc004d3853ff9e5a5db129ba9da617cb23e
Omelchenko Michael <l2maximum@mail.ru>
2019-12-28 21:28:44 +0300

    first commit

---
+++ chat/consumers.py
@@ -0,0 +1,46 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        self.send(text_data=json.dumps({
+            'event': "Send",
+            'message': message,
+            'username': username
+        }))
